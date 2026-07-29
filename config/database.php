a16c5d9c6e7a07d0ddbafe0e3511c697ab365bde
yhungxchange <yhungxchange@gmail.com>
2026-07-30 00:41:29 +0100

    Add database connection settings
    Updated database configuration with connection details.

--- config/database.php
+++ config/database.php
@@ -1,1 +1,9 @@
+<?php
 
+return [
+    'host' => 'dpg-d9l8rfu417fc73cefdrg-a',
+    'port' => '5432',
+    'database' => 'fin_core',
+    'username' => 'fin_core_user',
+    'password' => 'WwjK8M9CgsGojCE6gfNiAYTBVuzlFKDu'
+];
