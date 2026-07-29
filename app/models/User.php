3c54dbf5e8a189131da40621796301547f9bcac2
yhungxchange <yhungxchange@gmail.com>
2026-07-30 00:15:50 +0100

    Add User model with constructor and profile method

--- app/models/User.php
+++ app/models/User.php
@@ -1,1 +1,38 @@
+<?php
 
+class User
+{
+    public $id;
+    public $fullName;
+    public $username;
+    public $email;
+    public $phone;
+    public $passwordHash;
+    public $status;
+
+    public function __construct(
+        $fullName,
+        $username,
+        $email,
+        $phone,
+        $passwordHash
+    ) {
+        $this->fullName = $fullName;
+        $this->username = $username;
+        $this->email = $email;
+        $this->phone = $phone;
+        $this->passwordHash = $passwordHash;
+        $this->status = "active";
+    }
+
+    public function getProfile()
+    {
+        return [
+            "Full Name" => $this->fullName,
+            "Username" => $this->username,
+            "Email" => $this->email,
+            "Phone" => $this->phone,
+            "Status" => $this->status
+        ];
+    }
+}
