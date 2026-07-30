dcecea4d8da8a3c248597e81edcd7b8a7da8ea2d
yhungxchange <yhungxchange@gmail.com>
2026-07-30 02:47:57 +0100

    Create login.php

---
+++ views/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login - FinCore</title>
+</head>
+<body>
+
+<h2>Login</h2>
+
+<form method="POST">
+
+    <label>Email</label><br>
+    <input type="email" name="email" required><br><br>
+
+    <label>Password</label><br>
+    <input type="password" name="password" required><br><br>
+
+    <button type="submit">
+        Login
+    </button>
+
+</form>
+
+</body>
+</html>
