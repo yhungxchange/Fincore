70dea1b5958438f26a85925cfd2b0e467afacc0c
yhungxchange <yhungxchange@gmail.com>
2026-07-30 01:31:14 +0100

    Add registration form to register.php

---
+++ views/register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register - FinCore</title>
+</head>
+<body>
+
+<h2>Create Account</h2>
+
+<form method="POST">
+
+    <label>Full Name</label><br>
+    <input type="text" name="full_name" required><br><br>
+
+    <label>Username</label><br>
+    <input type="text" name="username" required><br><br>
+
+    <label>Email</label><br>
+    <input type="email" name="email" required><br><br>
+
+    <label>Phone</label><br>
+    <input type="text" name="phone" required><br><br>
+
+    <label>Password</label><br>
+    <input type="password" name="password" required><br><br>
+
+    <button type="submit">
+        Register
+    </button>
+
+</form>
+
+</body>
+</html>
