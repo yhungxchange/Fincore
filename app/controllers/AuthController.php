53b44f8a6b165e9d2f0f90ffec9cdcc0ee78c180
yhungxchange <yhungxchange@gmail.com>
2026-07-30 09:43:13 +0100

    Implement login method in AuthController

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -83,7 +83,38 @@ class AuthController
     }
 
     public function login()
-    {
-        echo "Login method reached!";
+{
+    $email = trim($_POST['email']);
+    $password = $_POST['password'];
+
+    if (empty($email) || empty($password)) {
+        die("Email and Password are required.");
     }
+
+    $stmt = $this->pdo->prepare("
+        SELECT *
+        FROM users
+        WHERE email = :email
+        LIMIT 1
+    ");
+
+    $stmt->execute([
+        'email' => $email
+    ]);
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (!$user) {
+        die("Invalid Email or Password.");
+    }
+
+    if (!password_verify($password, $user['password_hash'])) {
+        die("Invalid Email or Password.");
+    }
+
+    $_SESSION['user_id'] = $user['id'];
+    $_SESSION['username'] = $user['username'];
+
+    header("Location: dashboard.php");
+    exit;
 }
