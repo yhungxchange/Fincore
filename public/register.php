03cdae75eae237948b1f5920d709cb176770042d
yhungxchange <yhungxchange@gmail.com>
2026-07-30 17:19:03 +0100

    Add registration functionality in register.php

---
+++ public/register.php
@@ -0,0 +1,18 @@
+<?php
+
+$config = require __DIR__ . '/../config/database.php';
+
+require __DIR__ . '/../app/Database.php';
+require __DIR__ . '/../app/controllers/AuthController.php';
+
+$db = new Database($config);
+
+$pdo = $db->connection();
+
+$auth = new AuthController($pdo);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $auth->register();
+}
+
+require __DIR__ . '/../views/register.php';
