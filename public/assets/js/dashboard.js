64bcbe1f6d8a1842c56e6c6b1ec944962e0e9516
yhungxchange <yhungxchange@gmail.com>
2026-07-30 13:02:35 +0100

    Update dashboard.js

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+    const menuButton = document.querySelector(".menu-btn");
+    const sidebar = document.querySelector(".sidebar");
+
+    if (menuButton && sidebar) {
+
+        menuButton.addEventListener("click", function () {
+            sidebar.classList.toggle("active");
+        });
+
+    }
+
+});
