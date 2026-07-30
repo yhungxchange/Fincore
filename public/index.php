e80729fc97e2619fe1f18cf8fe4ed97ace8f2766
yhungxchange <yhungxchange@gmail.com>
2026-07-30 02:23:18 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,32 +3,16 @@
 $config = require __DIR__ . '/../config/database.php';
 
 require __DIR__ . '/../app/Database.php';
+require __DIR__ . '/../app/controllers/AuthController.php';
 
 $db = new Database($config);
 
 $pdo = $db->connection();
 
+$auth = new AuthController($pdo);
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    $fullName = trim($_POST['full_name']);
-    $username = trim($_POST['username']);
-    $email = trim($_POST['email']);
-    $phone = trim($_POST['phone']);
-    $password = $_POST['password'];
-
-    echo "<pre>";
-
-    print_r([
-        "Full Name" => $fullName,
-        "Username" => $username,
-        "Email" => $email,
-        "Phone" => $phone,
-        "Password" => $password
-    ]);
-
-    echo "</pre>";
-
-    exit;
+    $auth->register();
 }
 
 require __DIR__ . '/../views/register.php';
