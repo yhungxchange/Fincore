aaf2d3cdd7330e48fdab04ee6c3ea76d77f29bcd
yhungxchange <yhungxchange@gmail.com>
2026-07-30 01:55:44 +0100

    Handle registration form submission
    Capture and display registration form data on POST request.

--- public/index.php
+++ public/index.php
@@ -10,9 +10,25 @@ $pdo = $db->connection();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    echo "Registration form submitted!";
-    exit;
+    $fullName = trim($_POST['full_name']);
+    $username = trim($_POST['username']);
+    $email = trim($_POST['email']);
+    $phone = trim($_POST['phone']);
+    $password = $_POST['password'];
 
+    echo "<pre>";
+
+    print_r([
+        "Full Name" => $fullName,
+        "Username" => $username,
+        "Email" => $email,
+        "Phone" => $phone,
+        "Password" => $password
+    ]);
+
+    echo "</pre>";
+
+    exit;
 }
 
 require __DIR__ . '/../views/register.php';
