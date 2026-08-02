50e0d86e4615a0db5006dd468ae3d6149829a900
yhungxchange <yhungxchange@gmail.com>
2026-08-02 13:14:21 +0100

    Remove duplicate Font Awesome stylesheet link
    Removed duplicate Font Awesome stylesheet link.

--- views/cable-pin.php
+++ views/cable-pin.php
@@ -11,9 +11,8 @@
 
 <link rel="stylesheet" href="/assets/css/dashboard.css">
 <link rel="stylesheet" href="/assets/css/airtime-pin.css">
-
-<link rel="stylesheet"
-href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+  
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 
 </head>
 
