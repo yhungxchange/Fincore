c8db512e8425870221e41877f0c41286f392f132
yhungxchange <yhungxchange@gmail.com>
2026-08-02 05:40:10 +0100

    Fix HTML option generation in data.php

--- views/data.php
+++ views/data.php
@@ -305,8 +305,8 @@ const plans = dataPlans[network.value][this.value];
 
 plans.forEach(plan=>{
 
-dataPlan.innerHTML += `
-<option value="${plan[1]}">
+plan.innerHTML += `
+<option value="${plan[0]}|${plan[1]}">
 ${plan[0]} - ₦${plan[1]}
 </option>`;
 
