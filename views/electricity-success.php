87ff37aa24fd942519fcd514a89ea8aceffae844
yhungxchange <yhungxchange@gmail.com>
2026-08-02 17:00:10 +0100

    Update electricity-success.php

--- views/electricity-success.php
+++ views/electricity-success.php
@@ -139,7 +139,7 @@ Logout
 
 <div class="summary-item">
 <span>Meter Number</span>
-<strong><?= htmlspecialchars($electricity_meter_number) ?></strong>
+<strong><?= htmlspecialchars($meterNumber) ?></strong>
 </div>
 
 <div class="summary-item">
