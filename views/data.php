9683689e8167e8cacd38fd87c0edd7fa74d0cf95
yhungxchange <yhungxchange@gmail.com>
2026-08-02 05:23:59 +0100

    Update data.php

--- views/data.php
+++ views/data.php
@@ -306,7 +306,7 @@ const plans = dataPlans[network.value][this.value];
 plans.forEach(plan=>{
 
 plan.innerHTML += `
-<option value="${plan[0]}|${plan[1]}">
+<option value="${plan[1]}">
 ${plan[0]} - ₦${plan[1]}
 </option>`;
 
