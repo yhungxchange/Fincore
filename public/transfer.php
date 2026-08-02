2b7e0d8cc5b1c0152d4daf50c6846db568344492
yhungxchange <yhungxchange@gmail.com>
2026-08-02 19:44:28 +0100

    Refactor transfer.php by removing comments

--- public/transfer.php
+++ public/transfer.php
@@ -8,18 +8,14 @@ if (!isset($_SESSION['user_id'])) {
 }
 
 $config = require __DIR__ . '/../config/database.php';
+
 require __DIR__ . '/../app/Database.php';
 
 $db = new Database($config);
+
 $pdo = $db->connection();
 
 $userId = $_SESSION['user_id'];
-
-/*
-|--------------------------------------------------------------------------
-| Get Wallet Balance
-|--------------------------------------------------------------------------
-*/
 
 $stmt = $pdo->prepare("
 SELECT balance
@@ -35,11 +31,5 @@ $stmt->execute([
 $wallet = $stmt->fetch(PDO::FETCH_ASSOC);
 
 $balance = $wallet['balance'] ?? 0;
-
-/*
-|--------------------------------------------------------------------------
-| Load View
-|--------------------------------------------------------------------------
-*/
 
 require __DIR__ . '/../views/transfer.php';
