a0017e9c4e38fba14058a18db6cd5fdbada513bd
yhungxchange <yhungxchange@gmail.com>
2026-08-03 00:21:26 +0100

    Create transfer-success.php

---
+++ public/assets/css/transfer-success.php
@@ -0,0 +1,59 @@
+.success-card{
+
+text-align:center;
+
+}
+
+.success-icon{
+
+width:90px;
+
+height:90px;
+
+margin:0 auto 20px;
+
+border-radius:50%;
+
+background:#22c55e;
+
+display:flex;
+
+justify-content:center;
+
+align-items:center;
+
+color:#fff;
+
+font-size:42px;
+
+box-shadow:0 12px 25px rgba(34,197,94,.35);
+
+}
+
+.success-text{
+
+color:#666;
+
+margin-bottom:25px;
+
+}
+
+.summary-item{
+
+display:flex;
+
+justify-content:space-between;
+
+padding:15px 0;
+
+border-bottom:1px solid #eee;
+
+font-size:16px;
+
+}
+
+.summary-item strong{
+
+color:#111827;
+
+}
