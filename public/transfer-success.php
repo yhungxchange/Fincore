3870cf798a2203a5bc6e77f8b68500ae42b860ee
yhungxchange <yhungxchange@gmail.com>
2026-08-03 01:31:42 +0100

    Remove user_id check from transfer-success.php

--- public/transfer-success.php
+++ public/transfer-success.php
@@ -2,14 +2,11 @@
 
 session_start();
 
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit;
-}
-
 if (!isset($_SESSION['transfer_success'])) {
     header("Location: dashboard.php");
     exit;
 }
+
+$data = $_SESSION['transfer_success'];
 
 require __DIR__ . '/../views/transfer-success.php';
