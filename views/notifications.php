37a8bf3e44ab0bcfabce6e70594e67d3450f23c8
yhungxchange <yhungxchange@gmail.com>
2026-08-03 10:33:58 +0100

    Add sidebar navigation to notifications page

--- views/notifications.php
+++ views/notifications.php
@@ -20,7 +20,83 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
 
 <div class="dashboard">
 
-<?php include "sidebar.php"; ?>
+<!-- SIDEBAR -->
+
+<aside id="sidebar" class="sidebar">
+
+<div class="logo">
+
+<img src="/assets/images/logo.png">
+
+<h2>FinCore</h2>
+
+</div>
+
+<nav>
+
+<a href="dashboard.php" class="active">
+<i class="fa-solid fa-house"></i>
+Dashboard
+</a>
+
+<a href="wallet.php">
+<i class="fa-solid fa-wallet"></i>
+Wallet
+</a>
+
+<a href="fund-wallet.php">
+<i class="fa-solid fa-money-bill-wave"></i>
+Fund Wallet
+</a>
+
+<a href="transfer.php">
+<i class="fa-solid fa-money-bill-transfer"></i>
+Transfer
+</a>
+
+<a href="airtime.php">
+<i class="fa-solid fa-mobile-screen"></i>
+Airtime
+</a>
+
+<a href="data.php">
+<i class="fa-solid fa-wifi"></i>
+Data
+</a>
+
+<a href="cable.php">
+<i class="fa-solid fa-tv"></i>
+Cable TV
+</a>
+
+<a href="electricity.php">
+<i class="fa-solid fa-bolt"></i>
+Electricity
+</a>
+
+<a href="transactions.php">
+<i class="fa-solid fa-clock-rotate-left"></i>
+Transactions
+</a>
+
+<a href="notifications.php">
+<i class="fa-solid fa-bell"></i>
+Notifications
+</a>
+
+<a href="profile.php">
+<i class="fa-solid fa-user"></i>
+Profile
+</a>
+
+<a href="logout.php">
+<i class="fa-solid fa-right-from-bracket"></i>
+Logout
+</a>
+
+</nav>
+
+</aside>
 
 <main class="content">
 
