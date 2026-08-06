1cc017b4556b4ff6921f757517bab613a0fad1d9
yhungxchange <yhungxchange@gmail.com>
2026-08-06 14:19:04 +0100

    Create analytics.php

---
+++ public/analytics.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+}
+
+$config = require __DIR__ . '/../config/database.php';
+
+require __DIR__ . '/../app/Database.php';
+
+$db = new Database($config);
+
+$pdo = $db->connection();
+
+$userId = $_SESSION['user_id'];
+
+require __DIR__ . '/../views/analytics.php';
